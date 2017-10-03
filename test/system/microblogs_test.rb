236445e2eb99e6977bb5a276feab6ed575074795
Eyal Liebermann <eyal.liebermann@fiverr.com>
2017-10-03 11:47:49 +0000

    generater scaffold microblogs

---
+++ test/system/microblogs_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class MicroblogsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit microblogs_url
+  #
+  #   assert_selector "h1", text: "Microblog"
+  # end
+end
